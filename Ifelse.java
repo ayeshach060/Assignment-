4f1b924fc1034aaa59395501f466a5ffcb62b93f
ayesha <ayeshach060@gmail.com>
2020-12-07 18:56:48 +0500

    programs outputs

---
+++ Ifelse.java
@@ -0,0 +1,10 @@
+public class Ifelse {
+  public static void main(String[] args) {
+    int time = 15;
+    if (time < 18) {
+      System.out.println("Good day.");
+    } else {
+      System.out.println("Good evening.");
+    }  
+  }
+}
